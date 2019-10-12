e72301a287116f656b594db08ee787a58a03973d
Robert Hitchens <rjhitchens3@gmail.com>
2019-10-12 16:08:41 -0400

    h2 setup, console accessible

--- src/main/java/com/github/robhitchens/PMDBServer.java
+++ src/main/java/com/github/robhitchens/PMDBServer.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccessingDataJpaApplication {
+public class PMDBServer {
     public static void main(String[] args){
-        SpringApplication.run(AccessingDataJpaApplication.class, args);
+        SpringApplication.run(PMDBServer.class, args);
     }
 }
