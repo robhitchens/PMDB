d43fc16f781b5d18b354d8f303a11c708648b178
hitc6176 <rhitchens@wsfsbank.com>
2019-11-27 08:44:18 -0500

    added inversify for dependency injection

---
+++ src/service/MovieService.ts
@@ -0,0 +1,6 @@
+import MovieDao from "../dao/MovieDao";
+
+//TODO this doesn't really do anything more than the dao right now, but it will in future. maybe.
+export default class MovieService {
+
+}
