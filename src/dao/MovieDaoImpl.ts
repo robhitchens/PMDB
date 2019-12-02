575c6f6f4b93e9bbe5fcebefb643cb1604047883
hitc6176 <rhitchens@wsfsbank.com>
2019-12-02 14:59:08 -0500

    added some more code to moviedao

--- src/dao/MovieDaoImpl.ts
+++ src/dao/MovieDaoImpl.ts
@@ -11,6 +11,8 @@ import TYPES from "../constants/TYPES";
 import EntityManager from "./EntityManager";
 import {Logger} from "@overnightjs/logger";
 
+//TODO need to figure out how to handle transaction management later.
+//TODO need to figure out entity relationships.
 @injectable()
 export default class MovieDaoImpl implements MovieDao{
     private dataStore: DataStore;
@@ -22,13 +24,13 @@ export default class MovieDaoImpl implements MovieDao{
     public findMovieByTitle(title: string): Promise<Movie>{//TODO this may never be used
         return new Promise<Movie>(
             (resolve, reject) => {
-                this.dataStore.findOne({title: title}, (err: Error, document: any) => {
+                this.dataStore.findOne({title: title}, (err: Error, document: Movie) => {
                     if(err) {
                         Logger.Warn(`Error occurred finding movie for title: ${title}: error: ${err.message}`);
-                        Logger.Warn(err, true);
+                        Logger.Err(err, true);
                         reject(err);
                     }else {
-                        resolve(<Movie> document);
+                        resolve(document);
                     }
                 });
             }
@@ -37,13 +39,13 @@ export default class MovieDaoImpl implements MovieDao{
 
     findMoviesByFormat(format: string): Promise<Array<Movie>> {
         return new Promise((resolve, reject) => {
-            this.dataStore.find({format: format}, (err: Error, documents: Array<any>) => {
+            this.dataStore.find({format: format}, (err: Error, documents: Array<Movie>) => {
                 if(err){
                     Logger.Warn(`Error occurred getting movies for format ${format}. Error: ${err.message}`);
-                    Logger.Warn(err, true);
+                    Logger.Err(err, true);
                     reject(err);
                 }else{
-                    resolve(<Array<Movie>> documents);
+                    resolve(documents);
                 }
             });
         });
@@ -57,19 +59,43 @@ export default class MovieDaoImpl implements MovieDao{
 
     findMoviesByGenre(genre: string): Promise<Array<Movie>> {
         return new Promise((resolve, reject) => {
-            reject(new Error('TODO implement'));
+            this.dataStore.find({genre: genre}, (err: Error, documents: Array<Movie>) => {
+                if(err){
+                    Logger.Warn(`Error occurred finding movies by genre: ${genre}. Error: ${err.message}`);
+                    Logger.Err(err, true);
+                    reject(err);
+                }else{
+                    resolve(documents);
+                }
+            });
         });
     }
 
     findMoviesBySource(source: string): Promise<Array<Movie>> {
         return new Promise((resolve, reject) => {
-            reject(new Error('TODO implement'));
+            this.dataStore.find({source: source}, (err: Error, documents: Array<Movie>) => {
+                if(err){
+                    Logger.Warn(`Error occurred finding Movies for source: ${source}. Error: ${err.message}`);
+                    Logger.Err(err, true);
+                    reject(err);
+                }else{
+                    resolve(documents);
+                }
+            });
         });
     }
 
     findMoviesCloseToRunningTime(runningTime: string): Promise<Array<Movie>> {
         return new Promise((resolve, reject) => {
-            reject(new Error('TODO implement'));
+            this.dataStore.find({runningTime: runningTime}, (err: Error, documents: Array<Movie>) => {
+                    if(err){
+                        Logger.Warn(`Error occurred finding Movies by runningTime: ${runningTime}. Error: ${err.message}`);
+                        Logger.Err(err, true);
+                        reject(err);
+                    }else{
+                        resolve(documents);
+                    }
+            });
         });
     }
 
@@ -81,13 +107,13 @@ export default class MovieDaoImpl implements MovieDao{
 
     findMoviesMatchingTitle(title: string): Promise<Array<Movie>> {
         return new Promise((resolve, reject) => {
-                this.dataStore.find({title: title}, (err: Error, documents: Array<any>) => {
+                this.dataStore.find({title: title}, (err: Error, documents: Array<Movie>) => {
                     if(err){
                         Logger.Warn(`Error occurred finding movies by title  ${title}. Error: ${err.message}`);
                         Logger.Warn(err, true);
                         reject(err);
                     }else{
-                        resolve(<Array<Movie>> documents);
+                        resolve(documents);
                     }
                 });
             });
