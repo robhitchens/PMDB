5284053e5b58f0441a46993814277f7c394b2221
hitc6176 <rhitchens@wsfsbank.com>
2019-12-06 11:01:00 -0500

    added another test

--- test/service/MovieService.test.ts
+++ test/service/MovieService.test.ts
@@ -1,8 +1,10 @@
 import MovieServiceImpl from "../../src/service/MovieServiceImpl";
 import MovieService from "../../src/service/MovieService";
 import MovieDao from "../../src/dao/MovieDao";
+import BonesMovie from "../temp/BonesMovie";
 import {mocked} from "ts-jest/utils";
 import Movie from "../../src/entity/Movie";
+import bones from "../temp/BonesMovie";
 jest.mock('../../src/dao/MovieDao');
 
 let movieService: MovieService;
@@ -25,9 +27,29 @@ describe('When searching for a movie by title.', () => {
        movieService.getMovieByTitle("Bones")
            .catch((err: Error) => {
                errorThrown = true;
+               expect(mockMovieDao.findMovieByTitle).toHaveBeenCalled();
+               expect(mockMovieDao.findMovieByTitle).toHaveBeenCalledTimes(1);
                expect(err.message).toEqual(testErrorMessage);
            }).finally(() => {
                expect(errorThrown).toEqual(true);
        });
+   });
+   it('should return the requested movie', () => {
+        mockMovieDao.findMovieByTitle = jest.fn().mockImplementationOnce((title: string) => {
+            if(title === bones.title) {
+                return Promise.resolve(bones);
+            }else{
+                return Promise.reject(`title: ${title} no exist`);
+            }
+        });
+        movieService.getMovieByTitle("Bones")
+            .then((result) => {
+                expect(mockMovieDao.findMovieByTitle).toHaveBeenCalled();
+                expect(mockMovieDao.findMovieByTitle).toHaveBeenCalledTimes(1);
+                expect(result).not.toBeNull();
+                expect(result).toEqual(bones);
+            }).catch((err) => {//Note: error shouldn't be thrown here.
+                expect(err).toBeNull();
+        });
    });
 });
