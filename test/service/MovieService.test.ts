1fcecf5dc817ff3289dfd0f2165f7bc5ceca5379
hitc6176 <rhitchens@wsfsbank.com>
2019-12-06 10:10:42 -0500

    sort of working on unit tests

--- test/service/MovieService.test.ts
+++ test/service/MovieService.test.ts
@@ -1,5 +1,21 @@
 import MovieServiceImpl from "../../src/service/MovieServiceImpl";
+import MovieService from "../../src/service/MovieService";
+import MovieDao from "../../src/dao/MovieDao";
+import {mocked} from "ts-jest/utils";
+jest.mock('../../src/dao/MovieDao');
 
-beforeEach(() => {
+let movieService: MovieService;
+let mockMovieDao: MovieDao;
+
+beforeAll(() => {
+    movieService = new MovieServiceImpl(mockMovieDao);//Note constructor based dependency injection.
+});
+afterEach(() => {
     jest.clearAllMocks();
+});
+describe('When searching for a movie by title.', () => {
+   it('', () => {
+       //TODO need to figure out how to use ts-jest mocked. and mocking moviedao functions.
+       movieService.getMovieByTitle("Bones");
+   });
 });
