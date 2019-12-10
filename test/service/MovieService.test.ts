fb398d26d92e6ae86c75b8e9da62869a53f1da85
hitc6176 <rhitchens@wsfsbank.com>
2019-12-10 09:05:01 -0500

    fixed tests, refatored to use async await

--- test/service/MovieService.test.ts
+++ test/service/MovieService.test.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import MovieServiceImpl from "../../src/service/MovieServiceImpl";
 import MovieService from "../../src/service/MovieService";
 import MovieDao from "../../src/dao/MovieDao";
@@ -5,10 +6,11 @@ import BonesMovie from "../temp/BonesMovie";
 import {mocked} from "ts-jest/utils";
 import Movie from "../../src/entity/Movie";
 import bones from "../temp/BonesMovie";
-jest.mock('../../src/dao/MovieDao');
+import MovieDaoImpl from "../../src/dao/MovieDaoImpl";
+//jest.mock('../../src/dao/MovieDao');
 
 let movieService: MovieService;
-let mockMovieDao: MovieDao;
+let mockMovieDao: MovieDao = jest.genMockFromModule<MovieDao>('../../src/dao/MovieDaoImpl');
 
 beforeAll(() => {
     movieService = new MovieServiceImpl(mockMovieDao);//Note: constructor based dependency injection.
@@ -17,81 +19,70 @@ afterEach(() => {
     jest.clearAllMocks();
 });
 describe('When searching for a movie by title', () => {
-   it('returns a rejected promise', () => {
-       //TODO need to figure out how to use ts-jest mocked. and mocking moviedao functions.
-       let testErrorMessage: string = 'Test error';
-       mocked(mockMovieDao.findMovieByTitle)
-           .mockImplementationOnce(() => {
-           return Promise.reject(new Error(testErrorMessage));
-       });
-       let errorThrown: boolean = false;
-       movieService.getMovieByTitle("Bones")
-           .catch((err: Error) => {
-               errorThrown = true;
-               expect(mockMovieDao.findMovieByTitle).toHaveBeenCalled();
-               expect(mockMovieDao.findMovieByTitle).toHaveBeenCalledTimes(1);
-               expect(err.message).toEqual(testErrorMessage);
-           }).finally(() => {
-               expect(errorThrown).toBe(true);
-       });
-   });
-   it('should return the requested movie', () => {
-        mocked(mockMovieDao.findMovieByTitle)
+    it('returns a rejected promise', async () => {
+        //TODO need to figure out how to use ts-jest mocked. and mocking moviedao functions.
+        let testErrorMessage: string = 'Test error';
+        mockMovieDao.findMovieByTitle = jest.fn()
+            .mockImplementationOnce(() => {
+                return Promise.reject(new Error(testErrorMessage));
+            });
+
+        try {
+            await movieService.getMovieByTitle("Bones");
+        } catch (e) {
+            expect(mockMovieDao.findMovieByTitle).toHaveBeenCalled();
+            expect(mockMovieDao.findMovieByTitle).toHaveBeenCalledTimes(1);
+            expect(e.message).toEqual(testErrorMessage);
+        }
+    });
+    it('should return the requested movie', async () => {
+        mockMovieDao.findMovieByTitle = jest.fn()
             .mockImplementationOnce((title: string) => {
-            if(title === bones.title) {
-                return Promise.resolve(bones);
-            }else{
-                return Promise.reject(`title: ${title} no exist`);
-            }
-        });
-        movieService.getMovieByTitle("Bones")
-            .then((result) => {
-                expect(mockMovieDao.findMovieByTitle).toHaveBeenCalled();
-                expect(mockMovieDao.findMovieByTitle).toHaveBeenCalledTimes(1);
-                expect(result).not.toBeNull();
-                expect(result).toEqual(bones);
-            }).catch((err) => {//Note: error shouldn't be thrown here.
-                expect(err).toBeNull();
-        });
-   });
+                if (title === bones.title) {
+                    return Promise.resolve(bones);
+                } else {
+                    return Promise.reject(`title: ${title} no exist`);
+                }
+            });
+        let result: Movie = await movieService.getMovieByTitle("Bones");
+
+        expect(mockMovieDao.findMovieByTitle).toHaveBeenCalled();
+        expect(mockMovieDao.findMovieByTitle).toHaveBeenCalledTimes(1);
+        expect(result).not.toBeNull();
+        expect(result).toEqual(bones);
+
+    });
    afterEach(() => {
        mocked(mockMovieDao.findMovieByTitle).mockClear();
    });
 });
 
 describe('When getting all movies matching criteria', () => {
-    it('returns a rejected promise if an error occurs', () => {
+    it('returns a rejected promise if an error occurs', async () => {
         let errorMessage: string = 'Test error message.';
-        let errorThrown: boolean = false;
-        mocked(mockMovieDao.findMovies)
+        mockMovieDao.findMovies = jest.fn()
             .mockImplementationOnce(() => {
                 return Promise.reject(new Error(errorMessage));
             });
 
-        movieService.getMovies(bones)
-            .catch((error: Error) => {
-                errorThrown = true;
-                expect(mockMovieDao.findMovies).toHaveBeenCalled();
-                expect(mockMovieDao.findMovies).toHaveBeenCalledTimes(1);
-                expect(error.message).toBe(errorMessage);
-            })
-            .finally(() => {
-                expect(errorThrown).toBe(true);
-            });
+        try {
+            await movieService.getMovies(bones);
+        }catch (e) {
+            expect(mockMovieDao.findMovies).toHaveBeenCalled();
+            expect(mockMovieDao.findMovies).toHaveBeenCalledTimes(1);
+            expect(e.message).toBe(errorMessage);
+        }
     });
-    it('should return the an array of matching movies', () => {
-        mocked(mockMovieDao.findMovies)
+    it('should return the an array of matching movies', async () => {
+        mockMovieDao.findMovies = jest.fn()
             .mockImplementationOnce(() => {
             return Promise.resolve(new Array<Movie>(bones));
         });
-        movieService.getMovies(bones)
-            .then((results) => {
-                expect(mockMovieDao.findMovies).toHaveBeenCalled();
-                expect(mockMovieDao.findMovies).toHaveBeenCalledTimes(1);
-                expect(mockMovieDao.findMovies).toHaveBeenCalledWith(bones);
-                expect(results[0]).toEqual(bones);
-            })
-            .catch((err) => {expect(err).toBeNull()});
+        let results: Array<Movie> = await movieService.getMovies(bones);
+        expect(mockMovieDao.findMovies).toHaveBeenCalled();
+        expect(mockMovieDao.findMovies).toHaveBeenCalledTimes(1);
+        expect(mockMovieDao.findMovies).toHaveBeenCalledWith(bones);
+        expect(results[0]).toEqual(bones);
     });
     afterEach(() =>{
        mocked(mockMovieDao.findMovies).mockClear();
@@ -99,42 +90,41 @@ describe('When getting all movies matching criteria', () => {
 });
 
 describe('When update is called', () => {
-    it('should throw an error if it can\'t find the movie', () => {
+    it('should throw an error if it can\'t find the movie', async () => {
         let errorMessage: string = 'Test error message';
-        let errorThrown: boolean = false;
-        mocked(mockMovieDao.findMovieById).mockImplementationOnce((id: string) =>{
+        mockMovieDao.findMovieById = jest.fn()
+            .mockImplementationOnce((id: string) =>{
             console.debug(`mock findMovieById implementation called with: ${id}`);
             return Promise.reject(new Error(errorMessage));
         });
-        mocked(mockMovieDao.update).mockImplementationOnce((movie: Movie) => {
+        mockMovieDao.update = jest.fn()
+            .mockImplementationOnce((movie: Movie) => {
             console.debug(`mock update implementation called with:`, movie);
             return Promise.resolve(movie);
         });
-        movieService.update(bones)
-            .catch((err: Error) => {
-                errorThrown = true;
-                expect(mockMovieDao.findMovieById).toHaveBeenCalled();
-                expect(mockMovieDao.findMovieById).toHaveBeenCalledTimes(1);
-                expect(mockMovieDao.findMovieById).toHaveBeenCalledWith(bones._id);
-                expect(mockMovieDao.update).not.toHaveBeenCalled();
-                expect(err.message).toEqual(errorMessage);
-            })
-            .finally(() => {
-                expect(errorThrown).toBe(true);
-            })
-            ;
+        try {
+            await movieService.update(bones);
+        }catch (e) {
+            expect(mockMovieDao.findMovieById).toHaveBeenCalled();
+            expect(mockMovieDao.findMovieById).toHaveBeenCalledTimes(1);
+            expect(mockMovieDao.findMovieById).toHaveBeenCalledWith(bones._id);
+            expect(mockMovieDao.update).not.toHaveBeenCalled();
+            expect(e.message).toEqual(errorMessage);
+        }
     });//TODO add test for returning null
-    it('should return the updated movie', () => {
-        mocked(mockMovieDao.findMovieById).mockImplementationOnce((id: string) => {
+    it('should return the updated movie', async () => {
+        mockMovieDao.findMovieById = jest.fn()
+            .mockImplementationOnce((id: string) => {
             console.debug(`mock findMovieById implementation called with: ${id}`);
             return Promise.resolve(bones);
         });
-        mocked(mockMovieDao.update).mockImplementationOnce((movie: Movie) => {
+        mockMovieDao.update = jest.fn()
+            .mockImplementationOnce((movie: Movie) => {
             console.debug(`mock update implementation called with:`, movie);
             return Promise.resolve(movie);
         });
-        movieService.update(bones)
-            .then((result: Movie) => {
+        let result: Movie = await movieService.update(bones);
+
                 expect(mockMovieDao.findMovieById).toHaveBeenCalled();
                 expect(mockMovieDao.findMovieById).toHaveBeenCalledTimes(1);
                 expect(mockMovieDao.findMovieById).toHaveBeenCalledWith(bones._id);
@@ -142,7 +132,7 @@ describe('When update is called', () => {
                 expect(mockMovieDao.update).toHaveBeenCalledTimes(1);
                 expect(mockMovieDao.update).toHaveBeenCalledWith(bones);
                 expect(result).toEqual(bones);
-            });
+
     });
     afterEach(() => {
         mocked(mockMovieDao.findMovieById).mockClear();
