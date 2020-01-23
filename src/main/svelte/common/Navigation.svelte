71ef04bc22a5cea88a7a7066d5a11ea468ad5cf8
Robert Hitchens <rjhitchens3@gmail.com>
2020-01-22 19:11:55 -0500

    added on click events to navbar

--- src/main/svelte/common/Navigation.svelte
+++ src/main/svelte/common/Navigation.svelte
@@ -1,40 +1,44 @@
 <!--TODO need to figure out what layout will look like-->
-<script></script>
+<script>
+    let current = 'home';
+</script>
 
 <nav>
     <ul><!--TODO figure out what to put for navigation-->
-        <li><a class="active" href="#home">Home</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
+        <li><button class:active={current === 'home'} on:click={() => current = 'home'}>Home</button></li>
+        <li><button class:active={current === 'contact'} on:click={() => current = 'contact'}>Contact</button></li>
+        <li><button class:active={current === 'about'} on:click={() => current = 'about'}>About</button></li>
     </ul>
 </nav>
 
 <style>
     ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
 
-li {
-  float: left;
-}
+    li {
+        float: left;
+    }
 
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
+    li button {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        border: none;
+    }
 
-/* Change the link color to #111 (black) on hover */
-li a:hover {
-  background-color: #111;
-}
-.active {
-  background-color: #4CAF50;
-}
+    /* Change the link color to #111 (black) on hover */
+    li button:hover {
+        background-color: #111;
+        cursor: pointer;
+    }
+    .active {
+        background-color: #4CAF50;
+    }
 </style>
