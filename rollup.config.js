5d745be58d556bb1f50a760d04dc07b5f340b206
Robert Hitchens <rjhitchens3@gmail.com>
2020-01-28 19:21:12 -0500

    being dumb

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,9 @@ import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from "rollup-plugin-livereload";
 import {terser} from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import typescriptCompiler from "typescript";
+import sveltePreprocessor from "svelte-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
 //TODO apply configuration to project, need to get rollup and svelte working with typescript
@@ -19,6 +22,8 @@ export default {
         //replace({}),//TODO need to replace any environment keys in frontend with process configured keys.
         svelte({
             dev: !production,
+            extensions: [".svelte"],
+            preprocess: sveltePreprocessor(),
             css: css => {
                 css.write('public/build/bundle.css');
             }
@@ -27,8 +32,8 @@ export default {
             browser: true,
             dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
         }),
-        commonjs(),
-
+        commonjs({include: "node_modules/**"}),
+        typescript({typescript: typescriptCompiler}),
         //!production && serve(),
         !production && livereload('public'),
         production && terser()
